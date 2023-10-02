7e22d78b59d2a092cba629a370d3c0698d71c38b
X7REM <X7REM@ZIMA>
2023-10-02 20:04:08 -0300

    Agregar archivos de proyecto.

---
+++ proyecto-final-webconfig/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace proyecto_final_webconfig.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
