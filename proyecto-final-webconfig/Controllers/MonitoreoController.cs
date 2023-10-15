ac1c5d6e8a0dfbf42651dc7539aadf7f60053694
MPICON <mpicon2@up.edu.ar>
2023-10-14 23:36:01 -0300

    add monitoreo vista and controller with scaffold

---
+++ proyecto-final-webconfig/Controllers/MonitoreoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using proyecto_final_webconfig.Data;
+using proyecto_final_webconfig.Models;
+using proyecto_final_webconfig.Services;
+using System.Diagnostics;
+
+namespace proyecto_final_webconfig.Controllers
+{
+    public class MonitoreoController : Controller
+    {
+        private readonly ILogger<MonitoreoController> _logger;
+        private readonly IEventsService eventsService;
+
+        public MonitoreoController(ILogger<MonitoreoController> logger, IEventsService eventsService)
+        {
+            _logger = logger;
+            this.eventsService = eventsService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //get all entities from the database events
+            var events = await eventsService.GetAllRecentsEvents();
+
+            return View(events);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
