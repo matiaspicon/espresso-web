fff5cc488725c357e7705af3579f55e504e3c744
MPICON <mpicon2@up.edu.ar>
2023-10-29 10:51:31 -0300

    modify Events orderby timestamp descending

--- proyecto-final-webconfig/Repository/EventsRepository.cs
+++ proyecto-final-webconfig/Repository/EventsRepository.cs
@@ -15,12 +15,12 @@ namespace proyecto_final_webconfig.Repository
 
         public async Task<IEnumerable<Event>> GetAllRecentsEvents()
         {
-            return await espressoContext.Events.OrderByDescending(e => e.Timestamp).ToListAsync();
+            return await espressoContext.Events.OrderByDescending(e => e.Timestamp).Include(e => e.TypeDetection).ToListAsync();
         }
 
         public async Task<Event> GetEventByID(int id)
         {
-            return await espressoContext.Events.Where(x => x.Id == id).FirstAsync();
+            return await espressoContext.Events.Where(x => x.Id == id).Include(e => e.TypeDetection).FirstAsync();
         }
 
     }
