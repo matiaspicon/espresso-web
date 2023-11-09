00514a3641d84ffd9f6f10a9ef0a3f54c7406018
MPICON <mpicon2@up.edu.ar>
2023-11-09 02:09:24 -0300

    add config view and controller

---
+++ proyecto-final-webconfig/Controllers/ConfigController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using proyecto_final_webconfig.Data;
+using proyecto_final_webconfig.Models;
+using proyecto_final_webconfig.Repository;
+using proyecto_final_webconfig.Services;
+using System.Diagnostics;
+
+namespace proyecto_final_webconfig.Controllers
+{
+    public class ConfigController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IStatsRepository statsRepository;
+
+        public ConfigController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
